1d15e786df3cc813ead9aa7d08ebe2e0ce13138e
Lucas <lucass20586@gmail.com>
2021-01-26 11:34:02 -0300

    Código da Primeira Aula

---
+++ pages/index.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import db from '../db.json';
+
+import QuizBackground from '../src/components/QuizBackground';
+import Widget from '../src/components/Widget/index.js'
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+
+/*
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.secondary};
+` //? aqui ele trabalha com CSS dinâmico
+
+//* os dois são os mesmos, só que o de cima faz essa função debaixo dos panos
+
+// function Title(props) {
+//   // propcidades => propriedades
+//   return (
+//     // nesse caso, quando ele receber algo no Home(), essa função já recebe aquela propriedade
+//     <h1> 
+//       {props.children}
+//     </h1>
+//   )
+// }
+*/
+
+// TODO: Isso é JavaScript "empurrando o CSS"
+//* Configuração do Background
+const BackgroundImage = styled.div`
+  background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`; //? como se estivesse usando uma função que pode receber variáveis
+
+//* Configuração do Conteiner com o Quiz
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+// TODO: Isso é Next
+//! o next interpreta o Home() como a cara da nossa página
+export default function Home() {
+  return (
+    /*
+    //<Title>My page</Title>// toda fez que aparecer alguma tag que tem a letra maiúscula, representa que CRIAMOS essa tag 
+    */
+
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+              <h1>The Legend of Bhaskara</h1>
+          </Widget.Header>
+          <Widget.Content>  
+            <p>Será que você será mellhor do que Pitágoras ??</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+              <h1>Quizes da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>  
+            <p>Aqui alguns outros Quizes muito maneiros de alguns de meus colegas!!</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      <GitHubCorner projectUrl="" />
+      </QuizContainer>
+    </QuizBackground>
+
+  )
+  
+}
---
+++ src/components/Widget/index.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+// ! SEMPRE DEVE CHAMAR A BIBLIOTECA PARA QUE OS ELEMENTOS AQUI POSSAM RECEBER OS ATRIBUTOS
+
+//* Configuração do Widget do Quiz
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid ${({ theme }) => theme.colors.secondary};
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
+//* Configuração do Conteúdo dos Widget
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  
+  * {
+    margin: 0;
+  }
+`;
+
+//* Configuração do Conteúdo dos Widget
+  // O Widget.Content ajuda na organização da aplicação
+Widget.Content = styled.div` 
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+export default Widget;
