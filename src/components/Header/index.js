9b49ed1912f12142e0e0c476d41b9f00aa8bd64b
Lucas <lucass20586@gmail.com>
2021-01-26 15:18:21 -0300

    add icon and og:img

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Head from 'next/head';
-import db from '../db.json';
 
 export default function Header() {
   return (
@@ -12,20 +11,20 @@ export default function Header() {
         <meta name="title" content="The Legend of Bhaskara" />
         <meta name="description" content="Projeto de Quiz construído a partir da comunidade Alura." />
 
-        <link rel="icon" href="https://www.flaticon.com/svg/vstatic/svg/690/690326.svg?token=exp=1611675808~hmac=59ea373073f1b2410feb703edffba658" />
+        <link rel="icon" href="https://github.com/Lucas-Henrique-Lopes-Costa/bhaskaraquiz/blob/main/src/img/formula.png" />
         {/*  Open Graph / Facebook */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://bhaskaraquiz.lucas-henrique-lopes-costa.vercel.app/" />
         <meta property="og:title" content="The Legend of Bhaskara" />
         <meta property="og:description" content="Projeto de Quiz construído a partir da comunidade Alura." />
-        <meta property="og:image" content="{db.bg}" />
+        <meta property="og:image" content="https://github.com/Lucas-Henrique-Lopes-Costa/bhaskaraquiz/blob/1d15e786df3cc813ead9aa7d08ebe2e0ce13138e/src/image/background.jpg?raw=true" />
 
         {/* // Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://bhaskaraquiz.lucas-henrique-lopes-costa.vercel.app/" />
         <meta property="twitter:title" content="The Legend of Bhaskara" />
         <meta property="twitter:description" content="Projeto de Quiz construído a partir da comunidade Alura." />
-        <meta property="twitter:image" content="{db.bg}" />
+        <meta property="twitter:image" content="https://github.com/Lucas-Henrique-Lopes-Costa/bhaskaraquiz/blob/1d15e786df3cc813ead9aa7d08ebe2e0ce13138e/src/image/background.jpg?raw=true" />
       </Head>
     </div>
   );
